fb1f9999fbd44cde4d64d02f0cc3f13c0b2566c1
Dmitrii <dmitriilobanov3@gmail.com>
2026-03-17 11:12:31 -0500

    Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,18 @@
 import { createRoot } from "react-dom/client";
-import { Pizza } from "./Pizza";
+import Order from "./Order";
+import { StrictMode } from "react";
+import { PizzaOfTheDay } from "./PizzaOfTheDay";
 
 const App = () => {
   return (
     <div>
       <h1 className="logo">Padre Gino's Pizza</h1>
-      <Pizza
-        name="Pepperoni"
-        description="Mozzarella Cheese, Pepperoni"
-        image={"/public/pizzas/pepperoni.webp"}
-      />
-      <Pizza
-        name="The Hawaiian Pizza"
-        description="Sliced Ham, Pineapple, Mozzarella Cheese"
-        image={"/public/pizzas/hawaiian.webp"}
-      />
-      <Pizza
-        name="The Big Meat Pizza"
-        description="Bacon, Pepperoni, Italian Sausage, Chorizo Sausage"
-        image={"/public/pizzas/big_meat.webp"}
-      />
+      <Order />
+      <PizzaOfTheDay />
     </div>
   );
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(<StrictMode><App /></StrictMode>);
