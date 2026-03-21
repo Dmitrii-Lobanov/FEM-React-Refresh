3fbf612e88516c541ff267889f868c67eb856b95
Dmitrii <dmitriilobanov3@gmail.com>
2026-03-21 13:32:05 -0500

    React 19

--- src/routes/contact.lazy.jsx
+++ src/routes/contact.lazy.jsx
@@ -1,16 +1,28 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
 import { useMutation } from "@tanstack/react-query";
+import { useFormStatus } from "react-dom";
 import postContact from "../api/postContact";
 
 export const Route = createLazyFileRoute("/contact")({
   component: ContactRoute,
 });
 
+function ContactInput (props) {
+  const {pending} = useFormStatus();
+  return (
+    <input 
+      name={props.name}
+      placeholder={props.placeholder}
+      type={props.type}
+      disabled={pending}
+    />
+  );
+}
+
 function ContactRoute() {
   const {isSuccess, mutate} = useMutation({
-    mutationFn: function (e) {
-      e.preventDefault();
-      const formData = new FormData(e.target);
+    mutationFn: function (formData) {
+      "use server";
       return postContact(
         formData.get("name"),
         formData.get("email"),
@@ -25,9 +37,9 @@ function ContactRoute() {
       {isSuccess ? (
         <h3>Submitted!</h3>
       ) : (
-        <form onSubmit={mutate}>
-          <input name="name" placeholder="Name" />
-          <input type="email" name="email" placeholder="Email" />
+        <form action={mutate}>
+          <ContactInput type='text' name="name" placeholder="Name" />
+          <ContactInput type="email" name="email" placeholder="Email" />
           <textarea placeholder="Message" name="message"></textarea>
           <button>Submit</button>
         </form>
